44c4011cfe1ae084255ae453659f111b9bbe3c4f
TedrosGe <tedros.gebrenegus@gmail.com>
2022-08-04 21:25:58 -0400

    Frontend react started. Stock display on home page done

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from urllib.request import Request
 from  fastapi import BackgroundTasks, FastAPI, Request, Depends
 from fastapi.templating import Jinja2Templates
@@ -7,10 +8,24 @@ from models import Stock
 from database import SessionLocal, engine
 from pydantic import BaseModel
 import yfinance as yf
+# adding cors headers 
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
 models.Base.metadata.create_all(bind = engine)
 templates = Jinja2Templates(directory= "template")
-
+#adding cors url 
+origins = [
+    'http://localhost:3000'
+]
+#add middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = origins,
+    allow_credentials = True,
+    allow_methods = ["*"],
+    allow_headers = ["*"]
+)
 class StockRequest(BaseModel ):
     symbol :str
 #establish db connection 
@@ -31,16 +46,17 @@ def add_stock_data(id:int):
         stock.dividend_yield = int(yf_data.info['dividendYield']*100)
     db.add(stock)
     db.commit()
-   
+
+    return stock
 def stock_graph():
     pass
     
 
-@app.get("/")
-def home_page(request: StockRequest):
-    return templates.TemplateResponse("homepage.html", 
-    {"request": request   
-    })
+@app.get("/home")
+def home_page(request: Request, db: Session = Depends(get_db)):
+    
+    stocks = db.query(Stock).all()
+    return stocks
 
 
 @app.post("/stocks")
@@ -53,6 +69,7 @@ adds new stock to db
  stock.symbol = stock_request.symbol
  db.add(stock)
  db.commit()
+ 
  background_tasks.add_task(add_stock_data, stock.id)
  return {
      "code": "success",
